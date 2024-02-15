43d164e771693febac3560612902e8ca3b7b487d
Elizeu Moreira <126178625+ElizeuWinters@users.noreply.github.com>
2024-02-15 09:14:00 -0300

    Add files via upload

---
+++ back end 1 tri/loops.js
@@ -0,0 +1,15 @@
+let contador = 0;
+let parada = false;
+
+//Loop usando DO-WHILE
+while(parada){
+    console.log(`Loop while - $(contador)`);
+    contador++;
+}
+contador = 0;
+//Loop usando o DO-WHILE
+do {
+    console.log(`Loop Do - $(contador)`);
+    cantador++;
+}while(parada);
+cantador = 0;
---
+++ back end 1 tri/script.js
@@ -0,0 +1,13 @@
+let nome;
+nome = prompt(`Digiite o seu nome:`);
+console.log(nome);
+window.alert(`Olá ${nome} !`);
+let numero = 19;
+let palpite = prompt ("Digite um número de 0 a 20:");
+console.log(palpite);
+if (numero == palpie) {
+    window.alert(`Parabéns ${nome}! O número era ${número}, e você acertou!` );
+}
+else{
+    window.alert(`você`)
+}
